df28648758766bb485ae64920e283627e68dfb58
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-09-27 00:12:03 -0300

    Add 'not_yet_implemented' function to display a placeholder message

---
+++ src/prbr25/ui/not_yet_implemented.py
@@ -0,0 +1,14 @@
+from prbr25.ui.utils import clear_screen, print_centered
+
+
+def not_yet_implemented():
+    clear_screen()
+    print_centered("⚠️ This page is not implemented yet.⚠️")
+    print_centered("""                                                                                           
+,--.                                        ,--.       ,---.                          ,--. 
+|  |,-. ,--,--,  ,---.     ,--,--,  ,---. ,-'  '-.    /  .-' ,---. ,--.,--.,--,--,  ,-|  | 
+|     / |      \\| .-. |    |      \\| .-. |'-.  .-'    |  `-,| .-. ||  ||  ||      \' .-. | 
+|  \\  \\ |  ||  |' '-' '    |  ||  |' '-' '  |  |      |  .-'' '-' ''  ''  '|  ||  |\\ `-' | 
+`--'`--'`--''--'.`-  /     `--''--' `---'   `--'      `--'   `---'  `----' `--''--' `---'  
+                `---'                                                                      """)
+    input("Press Enter to go back...")
