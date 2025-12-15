d53b2b948526cdebc1ecafd9d1e7b881678857f0
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-12-14 23:29:23 -0300

    Add utility functions for event validation and dataframe display

--- src/prbr25/ui/utils.py
+++ src/prbr25/ui/utils.py
@@ -1,6 +1,12 @@
 import os
 import shutil
 
+from pandas import DataFrame
+from prbr25_logger.logger import setup_logger
+from tabulate import tabulate
+
+logger = setup_logger(__name__)
+
 
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
@@ -9,3 +15,26 @@ def clear_screen():
 def print_centered(text):
     width = shutil.get_terminal_size().columns
     print(text.center(width))
+
+
+def display_event_being_validated(event_info: DataFrame):
+    clear_screen()
+    logger.info(
+        f"{event_info.tournament_name.iloc[0]}: {event_info.event_name.iloc[0]}"
+    )
+    logger.info(
+        f"Localizacao: {event_info.city.iloc[0]}, {event_info.address_state.iloc[0]}"
+    )
+    logger.info(event_info.url.iloc[0])
+
+
+def display_dataframe(df: DataFrame, col_list: list = []):
+    if col_list:
+        df = df[col_list]
+    print(
+        tabulate(
+            df.values.tolist(),
+            headers=df.columns.tolist(),
+            tablefmt="grid",
+        )
+    )
