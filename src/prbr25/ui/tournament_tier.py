445d16dce4f14590b78416ba866e1ce4e06ae04e
Gabriel Medeiros <gabrielspmedeiros@gmail.com>
2025-12-21 17:35:59 -0300

    Enhance display_tournament_tier to include tournament score, attendee count, and DQ count

--- src/prbr25/ui/tournament_tier.py
+++ src/prbr25/ui/tournament_tier.py
@@ -3,10 +3,20 @@ from pyfiglet import figlet_format
 from prbr25.ui.utils import clear_screen
 
 
-def display_tournament_tier(tournament_name: str, event_name: str, weight_dict: dict):
+def display_tournament_tier(
+    tournament_name: str,
+    event_name: str,
+    weight_dict: dict,
+    tournament_score: int,
+    n_dqs: int,
+    n_attendees: int,
+):
     clear_screen()
     print(f"{tournament_name}: {event_name}")
-    art = get_char_art_tournament_weight(weight_dict)
+    print(f"Number of attendees: {n_attendees}")
+    print(f"Number of DQs: {n_dqs}")
+    print(f"Notable player bonus: {n_attendees - n_dqs}\n")
+    art = get_char_art_tournament_weight(weight_dict, tournament_score)
     print(art)
     input("Press Enter to go back...")
 
@@ -17,8 +27,11 @@ def generate_custom_char_art(text: str, char: str) -> str:
     return result
 
 
-def get_char_art_tournament_weight(weight_dict: dict) -> str:
+def get_char_art_tournament_weight(weight_dict: dict, tournament_score: int) -> str:
+    weight_string = (
+        f"{weight_dict['grade']}{weight_dict['Sign']}: {tournament_score}pts"
+    )
     if weight_dict["Sign"]:
-        return generate_custom_char_art(weight_dict["grade"], weight_dict["Sign"])
+        return generate_custom_char_art(weight_string, weight_dict["Sign"])
     else:
-        return generate_custom_char_art(weight_dict["grade"], weight_dict["grade"])
+        return generate_custom_char_art(weight_string, weight_dict["grade"])
